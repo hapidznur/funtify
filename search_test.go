8a0b37ffea34c20dfe1c76a79ca755612e9a28ae
Hafid Hidayatullah <hapidznur@gmail.com>
2020-11-06 01:12:40 +0700

    add test search

---
+++ search_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	server := testServerFromFile(http.StatusOK, "tests_data/search_track.txt")
+	client := testClient(server)
+	defer server.Close()
+
+	result, err := client.Search("tania bowra", SearchTypeArtist)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.Albums != nil {
+		t.Error("Searched for artists but received album results")
+	}
+	if result.Playlists != nil {
+		t.Error("Searched for artists but received playlist results")
+	}
+	if result.Tracks != nil {
+		t.Error("Searched for artists but received track results")
+	}
+	if result.Artists == nil || len(result.Artists.Artists) == 0 {
+		t.Error("Didn't receive artist results")
+	}
+	if result.Artists.Artists[0].Name != "Tania Bowra" {
+		t.Error("Got wrong artist name")
+	}
+}
